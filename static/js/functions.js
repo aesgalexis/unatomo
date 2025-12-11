22143975343407aa5eeaa6bb4ca524da76c8605e
aesgalexis <aesg.alexis@gmail.com>
2025-12-11 18:43:14 +0100

    Update functions.js

--- static/js/functions.js
+++ static/js/functions.js
@@ -1,1 +1,12 @@
+(function () {
+  const scrollTopButton = document.getElementById("scroll-top-button");
+  if (!scrollTopButton) return;
+
+  scrollTopButton.addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+})();
 
