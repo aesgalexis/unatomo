f76b3fd5d5415864174f293c0108d2136d2bef76
aesgalexis <aesg.alexis@gmail.com>
2025-12-24 12:00:09 +0100

    Update footer.js

--- static/js/footer.js
+++ static/js/footer.js
@@ -1,10 +1,8 @@
-  <script>
-    (function () {
-      const legalFooter = document.getElementById("legal-footer");
-      if (legalFooter) {
-        const year = new Date().getFullYear();
-        legalFooter.textContent =
-          "© " + year + " UNATOMO CORE SL · Todos los derechos reservados.";
-      }
-    })();
-  </script>
+(function () {
+  const legalFooter = document.getElementById("legal-footer");
+  if (legalFooter) {
+    const year = new Date().getFullYear();
+    legalFooter.textContent =
+      "© " + year + " UNATOMO CORE SL · Todos los derechos reservados.";
+  }
+})();
