9e21ec59af9d0166db5715b71ef19107532cc43c
aesgalexis <aesg.alexis@gmail.com>
2026-01-22 16:09:21 +0100

    Update site (2026-01-22 15:09:20)

---
+++ static/js/dashboard/index.js
@@ -0,0 +1,25 @@
+import { createStore } from "./store.js";
+import { createInitialState, reducer } from "./state.js";
+import { localStorageAdapter } from "./persistence/localStorage.js";
+import { createCanvas } from "./ui/canvas.js";
+import { createContextMenu } from "./ui/contextMenu.js";
+import { createInspector } from "./ui/inspector.js";
+
+const mount = document.getElementById("dashboard-mount");
+
+if (mount) {
+  const persisted = localStorageAdapter.load();
+  const initialState = createInitialState(persisted || {});
+  const store = createStore(reducer, initialState);
+
+  const { rect } = createCanvas(store, mount);
+  const toolbar = createContextMenu(store);
+  const inspector = createInspector(store);
+
+  rect.appendChild(toolbar);
+  rect.appendChild(inspector);
+
+  store.subscribe(() => {
+    localStorageAdapter.save(store.getState());
+  });
+}
---
+++ static/js/dashboard/types.js
@@ -0,0 +1,14 @@
+export const equipmentTypes = {
+  lavadora: {
+    id: "lavadora",
+    label: "Lavadora",
+    size: { w: 2, h: 2 },
+    defaults: { capacityKg: "" },
+  },
+  secadora: {
+    id: "secadora",
+    label: "Secadora",
+    size: { w: 2, h: 3 },
+    defaults: { capacityKg: "" },
+  },
+};
